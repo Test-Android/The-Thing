b65642f8dd2bc50a2aba2e345ae989fa8cd1a78c
Jake Cox <nicodangelo15@gmail.com>
2015-03-17 22:26:13 -0500

    Holy balls changes

--- TheThing/app/src/main/java/com/nicodangelo/thething/MainActivity.java
+++ TheThing/app/src/main/java/com/nicodangelo/thething/MainActivity.java
@@ -1,10 +1,12 @@
 package com.nicodangelo.thething;
 
+import android.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -12,6 +14,10 @@ import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity
 {
+    public static int oops = 30;
+    public static TextView x;
+    public static RelativeLayout layout;
+    public static int x1 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,8 +25,18 @@ public class MainActivity extends ActionBarActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        x = (TextView)findViewById(R.id.textView);
+        layout = (RelativeLayout)findViewById(R.id.Screen);
+    }
 
-
+    public static void MOVE(View view)
+    {
+        x.setLeft(x1 + 20);
+        float y = x.getX();
+        System.out.println(y);
+        layout.refreshDrawableState();
+        //x.setTop(30);
+        x.refreshDrawableState();
     }
 
 }
