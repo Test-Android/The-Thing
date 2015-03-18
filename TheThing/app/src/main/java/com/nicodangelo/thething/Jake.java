b19b01f758a3082cb020c808513eeeea07930364
Jake Cox <nicodangelo15@gmail.com>
2015-03-17 20:46:26 -0500

    made screen id

--- TheThing/app/src/main/java/com/nicodangelo/thething/Jake.java
+++ TheThing/app/src/main/java/com/nicodangelo/thething/Jake.java
@@ -1,6 +1,11 @@
 package com.nicodangelo.thething;
 
+import android.widget.TextView;
+
 public class Jake
 {
-
+    public static void JakeMethod()
+    {
+        TextView textView = (TextView)
+    }
 }
