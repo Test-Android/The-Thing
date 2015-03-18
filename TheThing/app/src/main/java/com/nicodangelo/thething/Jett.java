5c0945da5f6173c44d2fc92edc0a7be2441f6354
JK15825 <baseballranger@gmail.com>
2015-03-17 21:01:15 -0500

    oh wow so many words

--- TheThing/app/src/main/java/com/nicodangelo/thething/Jett.java
+++ TheThing/app/src/main/java/com/nicodangelo/thething/Jett.java
@@ -3,23 +3,26 @@ package com.nicodangelo.thething;
 /**
  * Created by Dad on 3/17/2015.
  */
+import android.graphics.Color;
+import android.widget.RelativeLayout;
+
 public class Jett
 {
-    public void changeColor()
+    public static void changeColor(RelativeLayout layout)
     {
         if(MainActivity.color == 0)
         {
-            Screen.setBackgroundColor(Color.black);
+            layout.setBackgroundColor(Color.BLACK);
             MainActivity.color++;
         }
         else if(MainActivity.color == 1)
         {
-            Screen.setBackgroundColor(Color.red);
+            layout.setBackgroundColor(Color.RED);
             MainActivity.color++;
         }
         else if( MainActivity.color == 2)
         {
-            Screen.setBackgroundColor(Color.blue);
+            layout.setBackgroundColor(Color.BLUE);
             MainActivity.color = 0;
         }
 
