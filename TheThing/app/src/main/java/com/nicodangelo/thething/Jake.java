88477d8f23503ebe68825b85f8ab7a3e88fb1f89
Jake Cox <nicodangelo15@gmail.com>
2015-03-17 20:51:58 -0500

    bloog

--- TheThing/app/src/main/java/com/nicodangelo/thething/Jake.java
+++ TheThing/app/src/main/java/com/nicodangelo/thething/Jake.java
@@ -1,11 +1,13 @@
 package com.nicodangelo.thething;
 
 import android.widget.TextView;
+import android.view.View;
 
 public class Jake
 {
+    private static TextView textView = (TextView) findViewById(R.id.textView);
     public static void JakeMethod()
     {
-        TextView textView = (TextView)
+
     }
 }
